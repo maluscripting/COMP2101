bf336c474844919b1d5eb7c31e5290bd0d46ca9f
maluscripting <malavikamanojan918@gmail.com>
2020-01-15 14:23:30 -0800

    first lab script files

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "hello world!"
+# my first script - helloworld.sh
+echo 'Hello World!'
+echo "I am process # $$"
---
+++ bash/helloworldtemplated.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# my second script - helloworldtemplated.sh
+
+cat <<EOF
+Hello World!
+I am process # $$
+EOF
